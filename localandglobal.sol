1c6291685d677ea60089a0872b57f5265cdc4a1b
Anish465 <99896276+Anish465@users.noreply.github.com>
2023-04-03 10:56:51 +0530

    Add files via upload

---
+++ localandglobal.sol
@@ -0,0 +1,26 @@
+        // SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract localandglobal 
+{
+    
+        function local() external pure returns (int product,int sum)
+        {
+            int a=5;
+            int b=2;
+            product=a*b;
+            sum=a+b;
+        }
+        function gloval() external view returns (address, uint,uint,bytes32) {
+
+        address owner=msg.sender;
+        uint timestamp=block.timestamp;
+        uint blocknumber=block.number;
+        bytes32 blckhash=blockhash(block.number);
+       return (owner,timestamp,blocknumber,blckhash);
+
+
+        
+    }
+    
+}
